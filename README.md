723333c6835d50d2691394125ea3b81b06cdcab1
Renzo Damian <renzo.damian.go@gmail.com>
2020-09-19 17:25:04 -0500

    r

---
+++ README.md
@@ -0,0 +1,7 @@
+# Evaluación de Desempeño A
+## Movimiento considerando la resistencia del aire
+### Objetivo
+Simular el movimiento de un proyectil considerando la resistencia del aire con condiciones iniciales.
+
+
+![alt text](./img/parabolico_img.png)
--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import math
 from matplotlib import pyplot
+# import sympy
+
+C_d = 0.5
+p = 1.200
+radio = 0.10
+A = math.pi * pow(radio, 2)
+y_max = 0
+v0 = 50
+theta = (45*math.pi) / 180
+m = 0.1
+X = []
+Y = []
+X_r = []
+Y_r = []
 
 
 class ball:
-    def __init__(self, v_o, angulo, masa):
+    def __init__(self, v_o, angulo, masa, C_d, p, A):
+        self.v_o = v_o
+        self.v = v_o
+        self.angulo = angulo
         self.v_x = v_o*math.cos(angulo)
         self.v_y = v_o*math.sin(angulo)
         self.m = masa
@@ -13,23 +30,34 @@ class ball:
         self.F = self.m*self.g
         self.x = 0
         self.y = 0
+        self.k = 0.5*C_d*p*A
+        self.c = v_o-((self.m*-self.g)/self.k)
 
     def move(self):
         self.v_x = self.v_x
-        self.v_y = self.v_y+(self.F/self.m)*self.dt
+        # self.v_y = self.v_y+self.g*self.dt
+        self.v_y = self.v_y+self.g*self.dt
         self.x = self.x + self.v_x*self.dt
-        self.y = self.y + self.v_y * self.dt
+        self.y = self.y + self.v_y*self.dt
+        self.t = self.t + self.dt
+        return self.x, self.y
+
+    def move_r(self):
+
+        self.v = (self.m*-self.g)/self.k + self.c * \
+            pow(math.e, (-self.k/self.m)*self.t)
+        print(self.v)
+        self.v_x = self.v*math.cos(self.angulo)
+        self.v_y = self.v*math.sin(self.angulo)
+        self.x = self.x + self.v_x*self.dt
+        self.y = self.y + self.v_y*self.dt
         self.t = self.t + self.dt
         return self.x, self.y
 
 
-y_max = 0
-v0 = 8
-theta = (45*math.pi) / 180
-m = 0.5
-X = []
-Y = []
-ball = ball(v0, theta, m)
+ball_o = ball(v0, theta, m, C_d, p, A)
+
+ball_r = ball(v0, theta, m, C_d, p, A)
 
 
 def max_H(v, angle):
@@ -40,7 +68,7 @@ def max_H(v, angle):
 
 
 while True:
-    x, y = ball.move()
+    x, y = ball_o.move()
 
     if y < 0:
         break
@@ -49,9 +77,20 @@ while True:
     X.append(x)
     Y.append(y)
 
+
+for _ in range(10):
+    x, y = ball_r.move_r()
+
+    # if y < 0:
+    #     break
+
+    X_r.append(x)
+    Y_r.append(y)
+
 print(max_H(v0, theta))
 print(y_max)
 
-pyplot.plot(X, Y)
-pyplot.axis('square')
+pyplot.plot(X_r, Y_r)
+# pyplot.axis('square')
+pyplot.savefig("img/parabolico_img.png")
 pyplot.show()
